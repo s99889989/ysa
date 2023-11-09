4f2bc521c0b9f3735c2496807f4d2c0459762e87
BuildTools <unconfigured@null.spigotmc.org>
2023-11-09 15:57:40 +0800

    2023/11/9

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-  <div id="main" class="dark:bg-black flex h-screen w-full flex-col items-center justify-center bg-white">
+  <div id="main" class="dark:bg-black flex container-top flex-col items-center justify-center bg-white">
     <h1 class="text-6xl dark:text-white font-semibold text-sky-400">2023</h1>
     <p class="mt-4 text-8xl dark:text-white font-bold text-gray-600">單身成人大會</p>
   </div>
