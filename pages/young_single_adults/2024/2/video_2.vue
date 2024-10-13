58f3ea6220c94f4c5504e3d0b6f6f55434322202
s99889989 <s02048898067@gmail.com>
2024-10-13 08:13:48 +0800

    2024/10/13

---
+++ pages/young_single_adults/2024/2/video_2.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+const path = ref(['ysa', 'video', '2024', '第2回', '第2天'])
+const title = ref('2024-第2回-第2天-影片')
+
+</script>
+
+<template>
+
+  <VideoDisplay  :path="path" :title="title" />
+
+</template>
+
+<style scoped>
+
+</style>
