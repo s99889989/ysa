c8b29271323b0689c93b534ec8e6c7954fdc3baa
s99889989 <s02048898067@gmail.com>
2024-10-13 08:32:43 +0800

    2024/10/13

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -157,7 +157,35 @@ const hidden_dropdown = () => {
             </div>
           </li>
 
+          <li>
+            <button id="dropdownNavbarLink" data-dropdown-toggle="ysa-2024" class="flex text-2xl items-center justify-between w-full py-2 pl-3 pr-4  text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+              2024<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg></button>
+            <!-- Dropdown menu -->
+            <div id="ysa-2024" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+              <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
 
+                <li aria-labelledby="dropdownNavbarLink">
+                  <button id="doubleDropdownButton" data-dropdown-toggle="ysa-2024-2" data-dropdown-placement="right-start" type="button" class="flex text-xl items-center justify-between w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                    第二回<svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                  </svg></button>
+                  <div id="ysa-2024-2" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="doubleDropdownButton">
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/photo_1" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第1天-照片</NuxtLink></li>
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/photo_2" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第2天-照片</NuxtLink></li>
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/photo_3" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第3天-照片</NuxtLink></li>
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/video_1" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第1天-影片</NuxtLink></li>
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/video_2" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第2天-影片</NuxtLink></li>
+                      <li><NuxtLink @click="hidden_dropdown" to="/young_single_adults/2024/2/video_3" class="block text-xl px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white">第3天-影片</NuxtLink></li>
+                    </ul>
+                  </div>
+                </li>
+
+              </ul>
+            </div>
+          </li>
 
 
         </ul>
